19159d1d57dfc8e7c946b119901433dcd260d055
Roshan <its.roshanojha@gmail.com>
2022-12-17 19:43:52 +0530

    getting started with the dropdown project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Dropdown from "./components/dropdown";
+
+function App() {
+
+  return (
+    <div>
+      <Dropdown />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/dropdown.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function dropdown() {
+  return (
+    <div>
+      dropdown
+    </div>
+  )
+}
+
+export default dropdown
